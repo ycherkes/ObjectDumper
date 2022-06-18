c0348f86642bcde75184a5ec248cc059d0334493
ycherkes <smurtdev@gmail.com>
2022-06-18 13:53:08 +0300

    Example how to bypass the UWP restrictions

---
+++ samples/uwp/TestUwp/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Windows.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace TestUwp
+{
+    /// <summary>
+    ///     An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var people = new[]
+            {
+                new { Name = "John", Age = 23 },
+                new { Name = "Stephen", Age = 32 }
+            };
+        }
+    }
+}
