a8e69898b8c067997c7ebfbb3920672f3abb69ef
Yevhen <ycherkes@outlook.com>
2022-06-21 19:04:17 +0300

    Extract all the project language-dependent stuff to the expression providers. (#7)
    Co-authored-by: ycherkes <smurtdev@gmail.com>

---
+++ src/ObjectDumper/DebuggeeInteraction/ExpressionProviders/IExpressionProvider.cs
@@ -0,0 +1,10 @@
+﻿namespace ObjectDumper.DebuggeeInteraction.ExpressionProviders
+{
+    internal interface IExpressionProvider
+    {
+        string GetStringTypeAssemblyLocationExpressionText();
+        string GetIsSerializerInjectedExpressionText();
+        string GetSerializedValueExpressionText(string expression, string format, string settings);
+        string GetLoadAssemblyExpressionText(string serializerFileName);
+    }
+}
