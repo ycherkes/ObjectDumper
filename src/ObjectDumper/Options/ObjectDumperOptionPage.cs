72b650ffa30ca7c384d20f87085dafc3103bfd65
ycherkes <smurtdev@gmail.com>
2022-06-30 20:26:27 +0300

    Fix default options values.

--- src/ObjectDumper/Options/ObjectDumperOptionPage.cs
+++ src/ObjectDumper/Options/ObjectDumperOptionPage.cs
@@ -11,32 +11,32 @@ namespace ObjectDumper.Options
         [DisplayName("Max Depth")]
         [Description("Max Depth")]
         [DefaultValue(25)]
-        public int CommonMaxDepth { get; set; }
+        public int CommonMaxDepth { get; set; } = 25;
 
         [Category(" Common Settings")]
         [DisplayName("Operation Timeout (Seconds)")]
         [Description("Operation Timeout in Seconds. Negative value means no timeout.")]
         [DefaultValue(10)]
-        public int CommonOperationTimeoutSeconds { get; set; }
+        public int CommonOperationTimeoutSeconds { get; set; } = 10;
 
         [Category("C#")]
         [DisplayName("Enabled")]
         [Description("Enabled")]
         [DefaultValue(true)]
-        public bool CSharpEnabled { get; set; }
+        public bool CSharpEnabled { get; set; } = true;
 
         [Category("C#")]
         [DisplayName("Ignore Null Values")]
         [Description("Ignore Null Values")]
         [DefaultValue(true)]
-        public bool CSharpIgnoreNullValues { get; set; }
+        public bool CSharpIgnoreNullValues { get; set; } = true;
 
 
         [Category("C#")]
         [DisplayName("Ignore Default Values")]
         [Description("Ignore Default Values")]
         [DefaultValue(true)]
-        public bool CSharpIgnoreDefaultValues { get; set; }
+        public bool CSharpIgnoreDefaultValues { get; set; } = true;
 
         [Category("C#")]
         [DisplayName("Use Full Type Name")]
@@ -48,67 +48,67 @@ namespace ObjectDumper.Options
         [DisplayName("DateTime Instantiation")]
         [Description("Configures how to DateTime, DateTimeOffset, TimeSpan will be instantiated")]
         [DefaultValue(DateTimeInstantiation.New)]
-        public DateTimeInstantiation CSharpDateTimeInstantiation { get; set; }
+        public DateTimeInstantiation CSharpDateTimeInstantiation { get; set; } = DateTimeInstantiation.New;
 
         [Category("C#")]
         [DisplayName("DateTime Kind")]
         [Description("DateTime Kind")]
         [DefaultValue(DateKind.ConvertToUtc)]
-        public DateKind CSharpDateKind { get; set; }
+        public DateKind CSharpDateKind { get; set; } = DateKind.ConvertToUtc;
 
         [Category("Json")]
         [DisplayName("Enabled")]
         [Description("Enabled")]
         [DefaultValue(true)]
-        public bool JsonEnabled { get; set; }
+        public bool JsonEnabled { get; set; } = true;
 
         [Category("Json")]
         [DisplayName("Ignore Null Values")]
         [Description("Ignore Null Values")]
         [DefaultValue(true)]
-        public bool JsonIgnoreNullValues { get; set; }
+        public bool JsonIgnoreNullValues { get; set; } = true;
 
         [Category("Json")]
         [DisplayName("Ignore Default Values")]
         [Description("Ignore Default Values")]
         [DefaultValue(true)]
-        public bool JsonIgnoreDefaultValues { get; set; }
+        public bool JsonIgnoreDefaultValues { get; set; } = true;
 
         [Category("Json")]
         [DisplayName("Naming Strategy")]
         [Description("Naming Strategy")]
         [DefaultValue(NamingStrategy.CamelCase)]
-        public NamingStrategy JsonNamingStrategy { get; set; }
+        public NamingStrategy JsonNamingStrategy { get; set; } = NamingStrategy.CamelCase;
 
         [Category("Json")]
         [DisplayName("Serialize Enums As Strings")]
         [Description("Serialize Enums As Strings")]
         [DefaultValue(true)]
-        public bool JsonSerializeEnumAsString { get; set; }
+        public bool JsonSerializeEnumAsString { get; set; } = true;
 
         [Category("Json")]
         [DisplayName("Type Name Handling")]
         [Description("Type Name Handling")]
         [DefaultValue(TypeNameHandling.None)]
-        public TypeNameHandling JsonTypeNameHandling { get; set; }
+        public TypeNameHandling JsonTypeNameHandling { get; set; } = TypeNameHandling.None;
 
         [Category("Visual Basic")]
         [DisplayName("Enabled")]
         [Description("Enabled")]
         [DefaultValue(true)]
-        public bool VisualBasicEnabled { get; set; }
+        public bool VisualBasicEnabled { get; set; } = true;
 
         [Category("Visual Basic")]
         [DisplayName("Ignore Null Values")]
         [Description("Ignore Null Values")]
         [DefaultValue(true)]
-        public bool VisualBasicIgnoreNullValues { get; set; }
+        public bool VisualBasicIgnoreNullValues { get; set; } = true;
 
         [Category("Visual Basic")]
         [DisplayName("Ignore Default Values")]
         [Description("Ignore Default Values")]
         [DefaultValue(true)]
-        public bool VisualBasicIgnoreDefaultValues { get; set; }
+        public bool VisualBasicIgnoreDefaultValues { get; set; } = true;
 
         [Category("Visual Basic")]
         [DisplayName("Use Full Type Name")]
@@ -120,43 +120,43 @@ namespace ObjectDumper.Options
         [DisplayName("DateTime Instantiation")]
         [Description("Configures how to DateTime, DateTimeOffset, TimeSpan will be instantiated")]
         [DefaultValue(DateTimeInstantiation.New)]
-        public DateTimeInstantiation VisualBasicDateTimeInstantiation { get; set; }
+        public DateTimeInstantiation VisualBasicDateTimeInstantiation { get; set; } = DateTimeInstantiation.New;
 
         [Category("Visual Basic")]
         [DisplayName("DateTime Kind")]
         [Description("DateTime Kind")]
         [DefaultValue(DateKind.ConvertToUtc)]
-        public DateKind VisualBasicDateKind { get; set; }
+        public DateKind VisualBasicDateKind { get; set; } = DateKind.ConvertToUtc;
 
         [Category("Xml")]
         [DisplayName("Enabled")]
         [Description("Enabled")]
         [DefaultValue(true)]
-        public bool XmlEnabled { get; set; }
+        public bool XmlEnabled { get; set; } = true;
 
         [Category("Xml")]
         [DisplayName("Ignore Null Values")]
         [Description("Ignore Null Values")]
         [DefaultValue(true)]
-        public bool XmlIgnoreNullValues { get; set; }
+        public bool XmlIgnoreNullValues { get; set; } = true;
 
         [Category("Xml")]
         [DisplayName("Ignore Default Values")]
         [Description("Ignore Default Values")]
         [DefaultValue(true)]
-        public bool XmlIgnoreDefaultValues { get; set; }
+        public bool XmlIgnoreDefaultValues { get; set; } = true;
 
         [Category("Xml")]
         [DisplayName("Naming Strategy")]
         [Description("Naming Strategy")]
         [DefaultValue(NamingStrategy.Default)]
-        public NamingStrategy XmlNamingStrategy { get; set; }
+        public NamingStrategy XmlNamingStrategy { get; set; } = NamingStrategy.Default;
 
         [Category("Xml")]
         [DisplayName("Serialize Enums As Strings")]
         [Description("Serialize Enums As Strings")]
         [DefaultValue(true)]
-        public bool XmlSerializeEnumAsString { get; set; }
+        public bool XmlSerializeEnumAsString { get; set; } = true;
 
         [Category("Xml")]
         [DisplayName("Use Full Type Name")]
@@ -168,13 +168,13 @@ namespace ObjectDumper.Options
         [DisplayName("Enabled")]
         [Description("Enabled")]
         [DefaultValue(true)]
-        public bool YamlEnabled { get; set; }
+        public bool YamlEnabled { get; set; } = true;
 
         [Category("Yaml")]
         [DisplayName("Naming Convention")]
         [Description("Naming Convention")]
         [DefaultValue(NamingConvention.Null)]
-        public NamingConvention YamlNamingConvention { get; set; }
+        public NamingConvention YamlNamingConvention { get; set; } = NamingConvention.Null;
 
         public string ToJson(string format)
         {
