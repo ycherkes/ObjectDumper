17d819a786f3d8d558e93987fd249397babb22c5
ycherkes <smurtdev@gmail.com>
2022-07-11 12:07:48 +0300

    code cleanup. (With big respect to Boris Johnson)

--- test/Serialization.UnitTests/RecordReferenceTypeSerializationTest.cs
+++ test/Serialization.UnitTests/RecordReferenceTypeSerializationTest.cs
@@ -7,25 +7,25 @@ namespace Serialization.UnitTests
         [Fact]
         public void SerializeRecordCsharp()
         {
-            var recordVar = new Person("Jeremy", "Clarkson");
+            var recordVar = new Person("Boris", "Johnson");
 
             var serializer = new CSharpSerializer();
 
             var result = serializer.Serialize(recordVar, null);
 
-            Assert.Equal("var person = new Person(\"Jeremy\", \"Clarkson\");\r\n", result);
+            Assert.Equal("var person = new Person(\"Boris\", \"Johnson\");\r\n", result);
         }
 
         [Fact]
         public void SerializeFlagsVb()
         {
-            var flagsVar = new Person("Jeremy", "Clarkson");
+            var recordVar = new Person("Boris", "Johnson");
 
             var serializer = new VisualBasicSerializer();
 
-            var result = serializer.Serialize(flagsVar, null);
+            var result = serializer.Serialize(recordVar, null);
 
-            Assert.Equal("Dim person = New Person(\"Jeremy\", \"Clarkson\")\r\n", result);
+            Assert.Equal("Dim person = New Person(\"Boris\", \"Johnson\")\r\n", result);
         }
 
         private record Person(string FirstName, string LastName);
