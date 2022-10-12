ab0cb7ee74db217453194c0f79abb628cde0752a
Yevhen <ycherkes@outlook.com>
2022-10-12 17:50:15 +0300

    Add DateOnly & TimeOnly serialization to C# & Vb (#29)
    * Add DateOnly & TimeOnly serialization to C# & Vb
    
    * Unit tests

---
+++ test/Serialization.UnitTests/DateTimeSerializationTest.cs
@@ -0,0 +1,102 @@
+using Newtonsoft.Json;
+using YellowFlavor.Serialization.Implementation;
+
+namespace Serialization.UnitTests
+{
+    public class DateTimeSerializationTest
+    {
+        [Fact]
+        public void SerializeDateOnlyCsharp()
+        {
+            var anonymous = new
+            {
+                DateOnly = DateOnly.ParseExact("2022-12-10", "O")
+            };
+
+            var serializer = new CSharpSerializer();
+
+            var result = serializer.Serialize(anonymous, JsonConvert.SerializeObject(new
+            {
+                UseFullTypeName = false,
+                DateTimeInstantiation = "Parse"
+            }));
+
+            Assert.Equal(
+@"var anonymousType = new 
+{
+    DateOnly = DateOnly.ParseExact(""2022-12-10"", ""O"")
+};
+", result);
+        }
+
+        [Fact]
+        public void SerializeTimeOnlyCsharp()
+        {
+            var anonymous = new
+            {
+                TimeOnly = TimeOnly.ParseExact("22:55:33.1220000", "O")
+            };
+
+            var serializer = new CSharpSerializer();
+
+            var result = serializer.Serialize(anonymous, JsonConvert.SerializeObject(new
+            {
+                UseFullTypeName = false,
+                DateTimeInstantiation = "Parse"
+            }));
+
+            Assert.Equal(
+@"var anonymousType = new 
+{
+    TimeOnly = TimeOnly.ParseExact(""22:55:33.1220000"", ""O"")
+};
+", result);
+        }
+
+        [Fact]
+        public void SerializeDateOnlyVb()
+        {
+            var anonymous = new
+            {
+                DateOnly = DateOnly.ParseExact("2022-12-10", "O")
+            };
+
+            var serializer = new VisualBasicSerializer();
+
+            var result = serializer.Serialize(anonymous, JsonConvert.SerializeObject(new
+            {
+                UseFullTypeName = false,
+                DateTimeInstantiation = "Parse"
+            }));
+
+            Assert.Equal(
+@"Dim anonymousType = New With {
+    .DateOnly = DateOnly.ParseExact(""2022-12-10"", ""O"")
+}
+", result);
+        }
+
+        [Fact]
+        public void SerializeTimeOnlyVb()
+        {
+            var anonymous = new
+            {
+                TimeOnly = TimeOnly.ParseExact("22:55:33.1220000", "O")
+            };
+
+            var serializer = new VisualBasicSerializer();
+
+            var result = serializer.Serialize(anonymous, JsonConvert.SerializeObject(new
+            {
+                UseFullTypeName = false,
+                DateTimeInstantiation = "Parse"
+            }));
+
+            Assert.Equal(
+@"Dim anonymousType = New With {
+    .TimeOnly = TimeOnly.ParseExact(""22:55:33.1220000"", ""O"")
+}
+", result);
+        }
+    }
+}
