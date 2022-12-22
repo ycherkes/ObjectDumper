9fb735248b3e43398e09dd83f42c1b57b19c3f02
ycherkes <ycherkes@outlook.com>
2022-12-22 13:11:19 +0200

    Put the license to the Chunk

--- src/Serialization/Embedded/CodeDom/EnumerableExtensions.cs
+++ src/Serialization/Embedded/CodeDom/EnumerableExtensions.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Took from: https://github.com/dotnet/runtime/blob/main/src/libraries/System.Linq/src/System/Linq/Chunk.cs
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
 using System.Collections.Generic;
 
 namespace YellowFlavor.Serialization.Embedded.CodeDom
