5dce6f6d555e503085220c5758b6182a867dfca8
ycherkes <ycherkes@outlook.com>
2023-04-04 17:38:54 +0200

    Info on this package for Help/About

---
+++ src/ObjectDumper/PackageConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace ObjectDumper
+{
+    internal static class PackageConstants
+    {
+        public const string Id = "75562b3a-ff38-4ad7-94f8-dc7f08140914";
+        public const string Version = "0.0.0.80";
+    }
+}
