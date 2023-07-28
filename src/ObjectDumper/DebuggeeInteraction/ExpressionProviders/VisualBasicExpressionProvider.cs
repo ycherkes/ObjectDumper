38a061a4fe1a0a697016419ae9c26045bb6c8f23
Yevhen Cherkes <ycherkes@outlook.com>
2023-07-28 19:41:17 +0200

    Fix the target framework determination for net framework
    AppDomainSetup.TargetFrameworkName is available for net framework 4.5 and higher
    https://learn.microsoft.com/en-us/dotnet/api/system.appdomainsetup.targetframeworkname?view=net-7.0#system-appdomainsetup-targetframeworkname
    AppContext.TargetFrameworkName is available for net framework  4.7
    https://learn.microsoft.com/en-us/dotnet/api/system.appcontext.targetframeworkname?view=net-7.0&viewFallbackFrom=netframework-4.5

--- src/ObjectDumper/DebuggeeInteraction/ExpressionProviders/VisualBasicExpressionProvider.cs
+++ src/ObjectDumper/DebuggeeInteraction/ExpressionProviders/VisualBasicExpressionProvider.cs
@@ -4,7 +4,7 @@
     {
         public string GetTargetFrameworkExpressionText()
         {
-            return "If(System.AppContext.TargetFrameworkName?.StartsWith(\".NETF\") <> False, If(System.AppContext.TargetFrameworkName, DirectCast(System.Reflection.Assembly.GetExecutingAssembly().GetCustomAttributes(GetType(System.Runtime.Versioning.TargetFrameworkAttribute), True)(0), System.Runtime.Versioning.TargetFrameworkAttribute).FrameworkName), System.AppContext.TargetFrameworkName?.Split(\",\")(0) & \",Version=v\" & System.Environment.Version.ToString)";
+            return "If(System.AppDomain.CurrentDomain?.SetupInformation?.TargetFrameworkName?.StartsWith(\".NETF\") <> False, If(System.AppDomain.CurrentDomain.SetupInformation.TargetFrameworkName, DirectCast(System.Reflection.Assembly.GetExecutingAssembly().GetCustomAttributes(GetType(System.Runtime.Versioning.TargetFrameworkAttribute), True)(0), System.Runtime.Versioning.TargetFrameworkAttribute).FrameworkName), System.AppDomain.CurrentDomain?.SetupInformation?.TargetFrameworkName?.Split(\",\")(0) & \",Version=v\" & System.Environment.Version.ToString)";
         }
 
         public string GetIsSerializerInjectedExpressionText()
