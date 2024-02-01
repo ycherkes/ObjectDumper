f9a4f131e55c55b5dd5027ebc7079205d33fb089
Yevhen Cherkes <ycherkes@outlook.com>
2024-02-01 07:57:37 +0100

    Fix the progress indication when error occurs during the dumping.

--- src/ObjectDumper/Recording/RecordedCommand.cs
+++ src/ObjectDumper/Recording/RecordedCommand.cs
@@ -14,26 +14,35 @@ internal sealed class RecordedCommand
     public void Run(Action<int, int, string> statusCallback = null)
     {
         statusCallback?.Invoke(1, 4, "Injecting serialization library...");
-        var (isInjected, injectionEvaluationResult) = InteractionService.InjectSerializer();
 
-        string data;
-        var isFileName = false;
-
-        statusCallback?.Invoke(2, 4, "Run serializer...");
-
-        if (isInjected)
+        try
         {
-            (data, isFileName) = InteractionService.Serialize(SelectionText, Format);
+            var (isInjected, injectionEvaluationResult) = InteractionService.InjectSerializer();
+
+            string data;
+            var isFileName = false;
+
+            statusCallback?.Invoke(2, 4, "Run serializer...");
+
+            if (isInjected)
+            {
+                (data, isFileName) = InteractionService.Serialize(SelectionText, Format);
+            }
+            else
+            {
+                data = injectionEvaluationResult;
+            }
+
+            statusCallback?.Invoke(3, 4, "Write result to output...");
+
+            DumpOutput.Write(Format, SelectionText, data, isFileName);
+
+            statusCallback?.Invoke(4, 4, "Serialization finished.");
         }
-        else
+        catch
         {
-            data = injectionEvaluationResult;
+            statusCallback?.Invoke(4, 4, "Error occurred.");
+            throw;
         }
-
-        statusCallback?.Invoke(3, 4, "Write result to output...");
-
-        DumpOutput.Write(Format, SelectionText, data, isFileName);
-
-        statusCallback?.Invoke(4, 4, "Serialization finished.");
     }
 }
