c472bf797e35c2effcd4a9bacbdbbbbf9056c492
Yevhen Cherkes <ycherkes@outlook.com>
2024-05-25 14:42:59 +0200

    Fix binary formatting by updating the VarDump library (#88)

--- src/ObjectDumper/PackageConstants.cs
+++ src/ObjectDumper/PackageConstants.cs
@@ -5,7 +5,7 @@ namespace ObjectDumper;
 internal static class PackageConstants
 {
     public const string Id = "75562b3a-ff38-4ad7-94f8-dc7f08140914";
-    public const string Version = "0.0.0.100";
+    public const string Version = "0.0.0.101";
     /// <summary>
     /// Command menu group (command set GUID).
     /// </summary>
